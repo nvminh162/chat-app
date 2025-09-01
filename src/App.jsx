7420ce460302d408a41364d5993f348b752e2ade
nvminh162 <nvminh162@gmail.com>
2025-09-01 10:30:18 +0700

    feat: initialize chat application with Vite, React, and Ant Design
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite configuration for React.
    - Created main application entry point (main.jsx) and integrated App component.
    - Developed Login component with Google and Facebook login buttons.
    - Added SVG assets for branding (Vite and React logos).
    - Set up basic styling with index.css.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Login from "./components/Login/index.jsx";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,28 @@
+import { Row, Col, Button, Typography } from 'antd';
+const { Title } = Typography;
+
+export default function Login() {
+  return (
+    <div>
+      <Row justify='center' style={{ height: 800 }}>
+        <Col span={8}>
+          <Title style={{ textAlign: 'center' }} level={3}>
+            Fun Chat
+          </Title>
+          <Button
+            style={{ width: '100%', marginBottom: 5 }}
+            // onClick={() => handleLogin(googleProvider)}
+          >
+            Đăng nhập bằng Google
+          </Button>
+          <Button
+            style={{ width: '100%' }}
+            // onClick={() => handleLogin(fbProvider)}
+          >
+            Đăng nhập bằng Facebook
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+}
